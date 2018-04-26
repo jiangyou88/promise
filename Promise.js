af7cfcbdd37be9a3a4991a67230048ba3b08f6b1
chenjuan88 <861594815@qq.com>
2018-04-27 00:03:49 +0800

    promise2018427

--- Promise.js
+++ Promise.js
@@ -3,27 +3,109 @@ function Promise(executor){//excutor是一个执行函数
     self.status="pending";
     self.value=undefined;//默认成功的值
     self.reason=undefined;//默认失败的原因
+    self.onResolvedCallbacks=[];//存放then成功的回调
+    self.onRejectedCallbacks=[];//存放then失败的回调
     function resolve(value){//成功状态
         if(self.status==="pending"){
             self.status="resolved";
             self.value=value;
+            self.onResolvedCallbacks.forEach(function(fn){
+                fn();
+            });
         }
     };
     function reject(reason){//失败状态
         if(self.status==="pending"){
             self.status="rejected";
             self.reason=reason;
+            self.onRejectedCallbacks.forEach(function(fn){
+                fn();
+            })
         }
     };
-    executor(resolve,reject);
+    try{
+        executor(resolve,reject);
+    }catch(e){//捕获的时候发生异常，就直接失败了
+        reject(e);
+    }
+    
+};
+function resolvePromise(p2,x,resolve,reject){
+    //有可能这里返回的x是别人的promise
+    //尽可能允许其他人乱写
+    if(p2===x){//这里应该报一个类型错误，有问题
+        return reject(new TypeError("循环引用了"));
+    }
+    //看x是不是一个promise，promise应该是一个对象
+    if(x!=null||(typeof x==="object"||typeof x==="function")){
+        //可能是promise {}，看这个对象中是否有then方法，如果有then我就认为他是promise了  
+        try{
+            let then=x.then;
+            if(typeof then==="function"){
+                then.call(x,function(y){
+                    //y可能还是一个promise，在去解析直到返回的是一个普通值
+                    resolvePromise(p2,x,resolve,reject);
+                },function (err) {
+                       reject(err); 
+                });
+            }else{
+                resolve(x);
+            }
+        }catch(e){
+            reject(e);
+        }
+
+    }else{//说明是一个普通值1
+        resolve(x);//表示成功了
+    }
 };
 Promise.prototype.then=function(onFulfilled,onRjected){
     let self=this;
+    let promise2;//返回的promise
     if(self.status==="resolved"){
-        onFulfilled(self.value);
+        promise2=new promise(function(resolve,reject){
+            //当成功或者失败执行时有异常那么返回的promise应该处于失败状态
+            //x可能是一个promise也有可能是一个普通的值
+            try{
+                let x=onFulfilled(self.value);
+                //x可能是别人promise，写一个方法统一处理
+                resolvePromise(promise2,x,resolve,reject);
+            }catch(e){
+                reject(e);//reject指的是promise2的失败
+            }; 
+        });
+        
     }
     if(self.status==="rejected"){
-        onRjected(self.reason);
+        promise2=new promise(function(resolve,reject){
+            try{
+                let x=onRjected(self.reason);
+                resolvePromise(promise2,x,resolve,reject);
+            }catch(e){
+                reject(e);
+            };           
+        });       
+    }
+    //当调用then时可能没成功，也没失败
+    if(self.status==="pending"){
+        promise2=new promise(function(resolve,reject){
+            self.onResolvedCallbacks.push(function(){
+                try{
+                    let x=onFulfilled(self.value);
+                    resolvePromise(promise2,x,resolve,reject);
+                }catch(e){
+                    reject(e);
+                }               
+            });
+            self.onRejectedCallbacks.push(function(){
+                try{
+                    let x=onRjected(self.reason);
+                    resolvePromise(promise2,x,resolve,reject);
+                }catch(e){
+                    reject(e);
+                }               
+            });
+        });     
     }
 };
 module.exports=Promise;
--- case.js
+++ case.js
@@ -1,10 +1,70 @@
 let Promise=require("./Promise");
 
-let p=new Promise(function(resolve,reject){
-    reject(100);
+//例子1
+// let p=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve(100);
+//     },1000);
+// });
+// p.then(function(data){
+//     console.log("data",data);
+// },function(err){
+//     console.log("err",err);
+// })
+
+
+//例子2
+let p=new Promise(function(resolve,reject){   
+    resolve("jiangyou");
 });
 p.then(function(data){
     console.log("data",data);
+    return "jiangyou2"
 },function(err){
     console.log("err",err);
+}).then(function(data){
+    console.log(data);
 })
+
+//1.promise实例可以多次then，当成功后会将then中的成功方法按顺序执行，我们可以将then中的成功的回调和失败的回调存到数组内，当成功时调用成功的数组即可
+
+//2.链式调用，jquery能实现链式调用靠的就是返回this。promise不能返回this,promise实现链式调用靠的是返回一个新的promise
+
+//3.如果then中无论是成功的回调还是失败的回调只要返回了结果就会走下一个then中的成功，如果有错误走下一个then 的失败
+
+//4.如果第一个promise返回一个普通值，会进到下一次then的成功的回调,如果第一个promise返回了一个promise，需要等待返回的promise执行后的结果传递给下一次then中
+
+//5.resolvePromise
+//返回的结果和promise是同一个那么永远不会成功和失败
+var p=new Promise(function(resolve,reject){
+    return p;
+});
+p.then(function(){
+    console.log(1)
+},function(err){
+    console.log(err)
+})
+
+//6.判断x是不是promise，如果x是对象并且x的then方法是函数我们就认为他是一个promise
+
+
+
+
+
+
+
+
+
+
+
+// let p=new Promise(function(resolve,reject){
+//     resolve();
+// });
+// let p2=p.then(function(){
+//     throw new Error("错误");
+// });
+// p2.then(function(){
+
+// },function(err){
+//     console.log(err);
+// })
